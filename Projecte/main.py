d06436b5a4d9758ebda3e80344bfa3f8a127ae50
Carlota <carlotafdezpdo@gmail.com>
2024-04-03 19:34:18 +0200

    Update main.py

--- Projecte/main.py
+++ Projecte/main.py
@@ -1,11 +1,69 @@
 # -*- coding: utf-8 -*-
+from pymongo import MongoClient
+import json
+from options import Options
+
+################################## PARAMETRES DE CONNEXIÓ ###################################
+mongoUser = ''
+mongoPassword = ''
+mongoDB = ''
+
 """
-Created on Fri Mar 15 18:53:42 2024
-
-@author: 
+1.  Actualitzeu els paràmetres del script NoSQLfromPython.py per poder establir la connexió amb el MongoDB del vostre PC.
 """
+# En execució remota
+Host = 'localhost'
+Port = 27017
+
+
+###################################### CONNEXIÓ ##############################################
+
+DSN = "mongodb://{}:{}".format(Host,Port)
+
+conn = MongoClient(DSN)
+
+
+############################# TRANSFERÈNCIA DE DADES AMB MONGO ##############################
+
+#Selecciona la base de dades a utilitzar --> test
+bd = conn['projecte']
+
+clients = bd['Clients']
+cotxes = bd['Cotxes']
+tiquets = bd['Tiquets']
+productes = bd['Productes']
+plaçapk = bd['Plaçapk']
+
+"""
+Carregar les dades des d'unfitxer JSON
+"""
+#Obrir Fitxer JSON
+# Parse options
+opts = Options()
+args = opts.parse()
+if args.fileName is not None:
+    with open(args.fileName) as f:
+        dades = json.load(f)
+
+    #Mostrem els continguts de Dades
+    # print(dades)
+
+for key, value in dades.items():
+    for doc in dades[key]:
+        if key == 'Clients':
+            clients.insert_one(doc)
+        elif key == 'Cotxes':
+            cotxes.insert_one(doc)
+        elif key == 'Tiquets':
+            tiquets.insert_one(doc)
+        elif key == 'Productes':
+            productes.insert_one(doc)
+        else:
+            plaçapk.insert_one(doc)
+
+print(clients)
 
 
 
-
-
+# Tanquem les connexions i el tunel
+conn.close()
